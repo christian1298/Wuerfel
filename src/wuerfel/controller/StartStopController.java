54c59f0f04fcff3b01d96e5d01dd633d17a23c89
christian1298 <christian.meier48@gmail.com>
2021-04-21 09:43:21 +0200

    start

---
+++ src/wuerfel/controller/StartStopController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wuerfel.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Christian
+ */
+public class StartStopController implements ActionListener
+{
+  public StartStopController()
+  {
+  
+  }
+  
+  public void RegisterEvents()
+  {
+    
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    
+  }
+}
